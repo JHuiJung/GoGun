7b4b4344ae0a718b95775e646fbeb6fce63a4943
JHuiJung <wjdgmlwnd12@naver.com>
2023-06-22 13:32:55 +0900

    06/22 work
    06/22 work

--- Assets/J_Folder/Scripts/PlayerController.cs
+++ Assets/J_Folder/Scripts/PlayerController.cs
@@ -1,51 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public enum player_status{
-    idle,
-    run,
-    sprint,
-    jump
-}
 
 public class PlayerController : MonoBehaviour
 {
     public float player_speed = 10f;
-    player_status p_status = player_status.idle;
+    public float jumpForce = 5f;
+    public float view_speed;
     Rigidbody rb;
     Animator animator;
     float hAxis;
     float vAxis;
+    
+    int jumpCnt = 0;
     bool isRun;
     bool isSpace;
     bool isSprint;
-    bool isGround;
+    bool isGround = true;
     Vector3 moveVec;
     // Start is called before the first frame update
     void Start()
     {
-        rb = this.GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
         animator = this.GetComponent<Animator>();
+        
     }
 
     // Update is called once per frame
+    private void FixedUpdate()
+    {
+        player_rotate();
+        player_Move();
+        Jump();
+    }
     void Update()
     {
         player_Input();
         player_state();
-        player_rotate();
-        player_Move();
+        
     }
     void player_rotate()
     {
-        Quaternion targetRotation = Quaternion.LookRotation(moveVec);
-        this.GetComponent<Transform>().rotation = targetRotation;
+        transform.LookAt(moveVec + transform.position);
     }
     void player_Input()
     {
         // 방향키 값을 입력 받아 
-        hAxis = Input.GetAxisRaw("Horizontal");
-        vAxis = Input.GetAxisRaw("Vertical");
+        hAxis = Input.GetAxis("Horizontal");
+        vAxis = Input.GetAxis("Vertical");
         isSpace = Input.GetAxisRaw("Jump") != 0 ? true : false;
         isSprint = Input.GetAxisRaw("Run") != 0 ? true : false;
         moveVec = new Vector3(hAxis, 0,vAxis).normalized;
@@ -54,21 +56,57 @@ public class PlayerController : MonoBehaviour
     }
     void player_state()
     {
-        animator.SetBool("isRun", isRun);
-        animator.SetBool("isSprint", isSprint);
+        
+        
         
     }
     void player_Move()
     {
 
-        if (isRun)
+        if (isRun && !isSprint)
         {
             transform.position += moveVec * player_speed * Time.deltaTime;
+            view_speed = player_speed;
+            animator.SetBool("isRun", isRun);
+
         }
-        else if (isSprint)
+        else if (isSprint && isRun)
         {
-            transform.position += moveVec * 1.5f * player_speed * Time.deltaTime;
+            transform.position += moveVec * 2f * player_speed * Time.deltaTime;
+            view_speed = player_speed*2;
+            animator.SetBool("isSprint", isSprint);
+        }
+
+        if (isGround)
+        {
+            animator.SetBool("isGround", isGround);
         }
         
+        
+        
+    }
+    void Jump()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space) && jumpCnt < 2)
+        {
+            jumpCnt++;
+            Debug.Log("점프 누름 :" + jumpCnt.ToString());
+            isGround = false;
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            animator.SetBool("isGround", isGround);
+
+        }
+        
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGround = true;  // 땅에 닿으면 isGrounded를 true로 설정
+            jumpCnt = 0;  // 점프 횟수 초기화
+
+        }
     }
 }
